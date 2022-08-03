15b036f401c6a7d444cd3d775de2e8a0c5960562
Jirka <jirka.borovec@seznam.cz>
2022-08-03 10:02:51 +0200

    datamodule

--- kaggle_volclassif/data/neck.py
+++ kaggle_volclassif/data/neck.py
@@ -1,9 +1,16 @@
+import glob
+import logging
 import os
-from typing import Union, Optional, Tuple
+from functools import partial
+from typing import Union, Optional, Tuple, Dict, Any
 
 import pandas as pd
 import torch
+from pytorch_lightning import LightningDataModule
 from torch.utils.data import Dataset
+from rising.loading import DataLoader
+
+from kaggle_volclassif.transforms import rising_resize
 
 
 class SpineScansDataset(Dataset):
@@ -12,14 +19,12 @@ class SpineScansDataset(Dataset):
         self,
         volume_dir: str = 'train_volumes',
         df_table: Union[str, pd.DataFrame] = 'train.csv',
-        vol_size: Optional[Tuple[int, int, int]] = None,
         mode: str = 'train',
         split: float = 0.8,
         in_memory: bool = False,
         random_state=42,
     ):
         self.volume_dir = volume_dir
-        self.vol_size = vol_size
         self.mode = mode
         self.in_memory = in_memory
 
@@ -42,7 +47,7 @@ class SpineScansDataset(Dataset):
 
         # populate images/labels
         self.label_names = sorted([c for c in self.table.columns if c.startswith("C")])
-        self.labels = self.table[self.label_names].values
+        self.labels = self.table[self.label_names].values if self.label_names else [None] * len(self.table)
         self.volumes = [os.path.join(volume_dir, f"{row['StudyInstanceUID']}.pt") for _, row in self.table.iterrows()]
         assert len(self.volumes) == len(self.labels)
 
@@ -62,3 +67,110 @@ class SpineScansDataset(Dataset):
 
     def __len__(self) -> int:
         return len(self.volumes)
+
+
+class SpineScansDM(LightningDataModule):
+
+    def __init__(
+        self,
+        data_dir: str = '.',
+        path_csv: str = 'train.csv',
+        vol_size: Optional[Tuple[int, int, int]] = (128, 128, 128),
+        in_memory: bool = False,
+        batch_size: int = 4,
+        num_workers: Optional[int] = None,
+        train_transforms=None,
+        valid_transforms=None,
+        split: float = 0.8,
+        **kwargs_dataloader,
+    ):
+        super().__init__()
+        # path configurations
+        assert os.path.isdir(data_dir), f"missing folder: {data_dir}"
+        self.train_dir = os.path.join(data_dir, 'train_volumes')
+        self.test_dir = os.path.join(data_dir, 'test_volumes')
+
+        if not os.path.isfile(path_csv):
+            path_csv = os.path.join(data_dir, path_csv)
+        assert os.path.isfile(path_csv), f"missing table: {path_csv}"
+        self.path_csv = path_csv
+
+        # other configs
+        self.vol_size = vol_size
+        self.batch_size = batch_size
+        self.split = split
+        self.in_memory = in_memory
+        self.num_workers = num_workers if num_workers is not None else os.cpu_count()
+        self.kwargs_dataloader = kwargs_dataloader
+
+        # need to be filled in setup()
+        self.train_dataset = None
+        self.valid_dataset = None
+        self.test_table = []
+        self.test_dataset = None
+        self.train_transforms = train_transforms
+        self.valid_transforms = valid_transforms
+
+    @property
+    def dl_defaults(self) -> Dict[str, Any]:
+        return dict(
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            sample_transforms=partial(rising_resize, size=self.vol_size),
+        )
+
+    def setup(self, *_, **__) -> None:
+        """Prepare datasets"""
+        ds_training = dict(
+            volume_dir=self.train_dir,
+            df_table=self.path_csv,
+            split=self.split,
+            in_memory=self.in_memory,
+        )
+        self.train_dataset = SpineScansDataset(**ds_training, mode='train')
+        logging.info(f"training dataset: {len(self.train_dataset)}")
+        self.valid_dataset = SpineScansDataset(**ds_training, mode='valid')
+        logging.info(f"validation dataset: {len(self.valid_dataset)}")
+
+        if not os.path.isdir(self.test_dir):
+            logging.warning(f"Missing test folder: {self.test_dir}")
+            return
+        ls_cases = [os.path.basename(p) for p in glob.glob(os.path.join(self.test_dir, '*'))]
+        self.test_table = [dict(StudyInstanceUID=n) for n in ls_cases]
+        self.test_dataset = SpineScansDataset(
+            self.test_dir,
+            df_table=pd.DataFrame(self.test_table),
+            split=0,
+            mode='test',
+        )
+        logging.info(f"test dataset: {len(self.test_dataset)}")
+
+    def train_dataloader(self) -> DataLoader:
+        return DataLoader(
+            self.train_dataset,
+            shuffle=True,
+            batch_transforms=self.train_transforms,
+            **self.dl_defaults,
+            **self.kwargs_dataloader,
+        )
+
+    def val_dataloader(self) -> DataLoader:
+        return DataLoader(
+            self.valid_dataset,
+            shuffle=False,
+            batch_transforms=self.valid_transforms,
+            **self.dl_defaults,
+            **self.kwargs_dataloader,
+        )
+
+    def test_dataloader(self) -> Optional[DataLoader]:
+        if not self.test_dataset:
+            logging.warning('no testing data found')
+            return
+        return DataLoader(
+            self.test_dataset,
+            shuffle=False,
+            batch_transforms=self.valid_transforms,
+            **self.dl_defaults,
+            **self.kwargs_dataloader,
+        )
